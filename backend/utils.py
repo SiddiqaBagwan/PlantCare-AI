9578f0e918a660b421f4463b0851619612f6a741
Siddiqa Bagwan <siddiqabagwan786@gmail.com>
2026-06-30 23:34:42 +0530

    improved utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,9 +1,10 @@
 from PIL import Image
 
+ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png"}
+
 def load_image(file):
-
     image = Image.open(file)
+    return image.convert("RGB")
 
-    image = image.convert("RGB")
-
-    return image
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
